2e699a59e2d30b50d215122db9c11192b324cb2c
Sylvie-B <unsolo@hotmail.fr>
2021-11-19 12:09:45 +0100

    review css, create js

---
+++ app100.js
@@ -0,0 +1,22 @@
+//  get modal elements
+let instruction = document.getElementById('instruction');
+let win = document.getElementById('win');
+let loose = document.getElementById('loose');
+
+// get display elements
+let tryNbr = document.getElementById('tryNbr');
+let nbrTest = document.getElementById('case').getElementsByTagName('span');
+let upOrDown = document.getElementById('upOrDown');
+let arrow = document.getElementById('arrow').querySelector("div");
+
+// get input
+let nbr = document.getElementById('nbr').value;
+
+// get click element
+let start = document.getElementById('start');
+
+// game
+// start.addEventListener('click', function (){
+//     instruction.style.display = "none";
+// })
+
