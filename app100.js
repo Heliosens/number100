99ea1644cce5aa1ea49c82796551c26d936b766c
Sylvie-B <unsolo@hotmail.fr>
2021-11-19 13:22:09 +0100

    get element js

--- app100.js
+++ app100.js
@@ -9,14 +9,25 @@ let nbrTest = document.getElementById('case').getElementsByTagName('span');
 let upOrDown = document.getElementById('upOrDown');
 let arrow = document.getElementById('arrow').querySelector("div");
 
-// get input
-let nbr = document.getElementById('nbr').value;
-
 // get click element
 let start = document.getElementById('start');
+let test = document.getElementById('testBtn');
 
 // game
-// start.addEventListener('click', function (){
-//     instruction.style.display = "none";
-// })
+// number to find
+let nbrToFind = Math.ceil(Math.random() * 100)
+console.log(nbrToFind);
 
+start.addEventListener('click', function (){
+    instruction.style.display = "none";
+})
+
+test.addEventListener('click', function (){
+    // get input value
+    let nbr = parseInt(document.getElementById('nbr').value);
+    console.log(nbr);
+    // check
+    if(nbrToFind === nbr){
+        win.style.display = "flex";
+    }
+})
