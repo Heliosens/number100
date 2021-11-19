d599f95dc5264bd89b38cbb6fc1d4cd22c54b3db
Sylvie-B <unsolo@hotmail.fr>
2021-11-19 16:55:12 +0100

    test responsive

--- app100.js
+++ app100.js
@@ -1,14 +1,14 @@
 //  get modal elements
 let instruction = document.getElementById('instruction');
-let win = document.getElementById('win');
-let loose = document.getElementById('loose');
+let info = document.getElementById('info');
 
 // get display elements
 let tryNbrDisplay = document.getElementById('tryNbr');
 let nbrTestFrame = document.getElementById('case').getElementsByTagName('span');
 let upOrDown = document.getElementById('upOrDown');
 let arrow = document.getElementById('arrow').querySelector("div");
-let actualNbr = document.getElementsByClassName('nbr');
+let actualNbr = document.getElementById('nbr');
+let wOrL = document.getElementById('weenOrLoos');
 
 // get click element
 let start = document.getElementById('start');
@@ -22,30 +22,31 @@ let tryNbr = 0;
 let nbrToFind = 0;
 function randomNbr (){
     nbrToFind = Math.ceil(Math.random() * 100);
-    actualNbr[0].innerHTML = nbrToFind.toString();
-    actualNbr[1].innerHTML = nbrToFind.toString();
+    actualNbr.innerHTML = nbrToFind.toString();
     console.log(nbrToFind);
 }
 
 // start
 start.addEventListener('click', function (){
+    document.getElementById("inNbr").focus();
     instruction.style.display = "none";
-
     randomNbr();
 })
 
 // test
 test.addEventListener('click', function (){
     // get input value
-    let nbr = parseInt(document.getElementById("nbr").value);
-    document.getElementById("nbr").value = "";
-    if(isNaN(nbr) || nbr < 0 || nbr > 100 ){
+    let nbr = parseInt(document.getElementById("inNbr").value);
+    console.log("nbr = " + nbr);
+    document.getElementById("inNbr").value = "";
+    if(isNaN(nbr) || nbr <= 0 || nbr > 100 ){
         alert('veuillez entrer un nombre entre 1 et 100');
     }
     else {
         // check
         if(nbrToFind === nbr){
-            win.style.display = "flex";
+            wOrL.innerHTML = "Vous avez gangné !!!"
+            info.style.display = "flex";
             reStart.addEventListener('click', function (){
                 // game
                 randomNbr()
@@ -55,20 +56,26 @@ test.addEventListener('click', function (){
                 }
                 tryNbr = 0;
                 tryNbrDisplay.innerHTML = tryNbr.toString();
-                win.style.display = 'none';
+                info.style.display = 'none';
             })
         }
         else if (nbrToFind > nbr){
+            nbrTestFrame[tryNbr].innerHTML = nbr.toString();
             tryNbr++;
             upOrDown.innerHTML = "plus grand";
             arrow.style.backgroundImage = 'url("arrow-up-6-xxl.png")';
         }
         else {
+            nbrTestFrame[tryNbr].innerHTML = nbr.toString();
             tryNbr++;
             upOrDown.innerHTML = "plus petit";
             arrow.style.backgroundImage = "url('arrow-down-6-xxl.png')";
         }
+        tryNbrDisplay.innerHTML = tryNbr.toString();
+        if(tryNbr === 10){
+            wOrL.innerHTML = "Vous avez perdu !!!"
+            info.style.display = "flex";
+        }
     }
-
 })
 
